2fa9746fcde20edc2c86c9f34e3a578a6ee36871
th168 <t.h.his.story@gmail.com>
2023-04-19 00:07:42 +0900

    Two Fadeins

--- contents/script_2.js
+++ contents/script_2.js
@@ -1,17 +1,33 @@
-let fadeIns = document.querySelectorAll('.fadein-before');
+let fadeIns_Odd = document.querySelectorAll('.fadein-before_Odd');
+let fadeIns_Even = document.querySelectorAll('.fadein-before_Even');
 
-for (let i = 0; i < fadeIns.length; i++){
+for (let i = 0; i < fadeIns_Odd.length; i++){
     window.addEventListener('scroll', ()=>{
-        const elmTop = fadeIns[i].getBoundingClientRect().top;
+        const elmTop = fadeIns_Odd[i].getBoundingClientRect().top;
         const scroll = window.pageYOffset;
         const offset = elmTop + scroll;
         const windowHeight = window.innerHeight; 
         
         if(scroll > offset - windowHeight){
-            fadeIns[i].classList.add('fadein-after');
+            fadeIns_Odd[i].classList.add('fadein-after_Odd');
         }
         else{
-            fadeIns[i].classList.remove('fadein-after');
+            fadeIns_Odd[i].classList.remove('fadein-after_Odd');
+        }
+    })
+};
+for (let i = 0; i < fadeIns_Even.length; i++){
+    window.addEventListener('scroll', ()=>{
+        const elmTop = fadeIns_Even[i].getBoundingClientRect().top;
+        const scroll = window.pageYOffset;
+        const offset = elmTop + scroll;
+        const windowHeight = window.innerHeight; 
+        
+        if(scroll > offset - windowHeight){
+            fadeIns_Even[i].classList.add('fadein-after_Even');
+        }
+        else{
+            fadeIns_Even[i].classList.remove('fadein-after_Even');
         }
     })
 };
